de00a6525949db67c3467c568084cb4fbfcdc9b5
gwentinho <qschroed@gmail.com>
2021-10-27 11:16:09 +0200

    more rpg stuff

--- rpg.ml
+++ rpg.ml
@@ -3,8 +3,9 @@ Random.self_init ()
 type charClass =  Ranger | Warrior | Magician
 type charObject = GoldCoins | ChickenWings | Sponges
 type charBag = (charObject * int) list
-type race = Golem | Boar | MosquitoesSwarm
+type race = Golem | Boar | MosquitoesSwarm of int
 
+exception Death of string
 
 type character = {
   cls: charClass;
@@ -36,7 +37,30 @@ let random_monster () =
   match x with
   | 0 -> Golem
   | 1 -> Boar
-  | 2 -> MosquitoesSwarm
+  | 2 -> MosquitoesSwarm (Random.int 10)
   | _ -> Golem (* shouldnt be able to happen*)
 
-let hit (c:character) = (*for next time*)
+let hit (c:character) =
+  let damage, chance = match c.cls with
+    | Warrior -> (10, 0.3)
+    | Ranger -> (4, 0.7)
+    | Magician -> (5,0.5) in
+  let roll = Random.float 1.0 in
+    if roll < (chance +. 0.05 *. (float_of_int c.xp)) then damage else 0
+
+let monster_hit (m: monster) =
+  let r = m.rc in
+  match r with
+    | Golem -> 4
+    | MosquitoesSwarm n ->  n / 2 (*no idea how to make it swarm*)
+    | Boar -> 2
+
+let fight (c: character) (m: monster) =
+  let battleOne = m.hp - (hit c) in
+  if battleOne <= 0 then {c with xp = c.xp + 5}
+  else let battleTwo = c.hp - (monster_hit m) in
+  if battleTwo <= 0 then raise (Death "The monster killed you") else {c with hp = battleTwo; xp = c.xp + 5}
+
+let unfortunate_encounter (c: character) =
+  let m = random_monster () in
+  m (*finish this next time*)
