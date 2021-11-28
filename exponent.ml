96b97f3855346816f6af76be3ee16b0595ab70bd
Gwentinho <qschroed@gmail.com>
2021-11-28 23:26:09 +0100

    implemented fast exponentiation

--- exponent.ml
+++ exponent.ml
@@ -2,8 +2,44 @@ let rec expNaive x n = if n = 0 then 1 else x * expNaive x (n-1)
 
 let rec expTemp x n a = if n = 0 then a else expTemp x (n-1) (a * x)
 
-let expTR x n = expTemp x n 1
+let expTR x n =
+  let rec aux a = function
+    | 0 -> a
+    | m -> aux (a * x) (m - 1) in
+  aux 1 n
 
 
-let _ = Printf.printf "%d\n" (expTR 10 5)
-let _ = Printf.printf "%d\n" (expNaive 10 5)
+
+(*note that this algo gladly ignores negative values :)*)
+(*creates list of bits with most significant bit at the front*)
+let bitsFromInt x =
+  let rec aux l x = match x with
+    | 0 -> l
+    | x ->
+      let lowest = x mod 2 in
+      aux (lowest::l) (x/2) in
+  aux [] x
+
+let fastExp x n =
+  let expBits = bitsFromInt n in
+  let rec aux acc e = match e with
+    | [] -> acc
+    | h::t ->
+      let accSqr = acc * acc in
+      let next = accSqr * (if h == 1 then x else 1) in
+      aux next t in
+  aux 1 expBits
+
+
+let fastExpMod x n m =
+  let expBits = bitsFromInt n in
+  let rec aux acc e = match e with
+    | [] -> acc
+    | h::t ->
+      let accSqr = (acc * acc) mod m in
+      let next = (accSqr * (if h == 1 then x else 1)) mod m in
+      aux next t in
+  aux 1 expBits
+
+let _ = Printf.printf "%d\n" (fastExpMod 200023 72013 541) (*should output 121*)
+
